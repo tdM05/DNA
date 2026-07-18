f7fc45b15f5914b2a9659e06ef6c12e019ca1baf
Tadd Yuchi Mao <44916381+tdM05@users.noreply.github.com>
2026-07-18 17:10:08 -0400

    save

--- LeanEuclidPlus/Book1/Prop43/step1.lean
+++ LeanEuclidPlus/Book1/Prop43/step1.lean
@@ -1,1 +1,17 @@
-p
+import SystemE
+import Book1.Prop34.Main
+set_option linter.unusedVariables false
+set_option linter.unnecessarySeqFocus false
+
+namespace Elements.Book1
+
+set_option systemE.solverTime 30 in
+theorem helper_1_43_step1
+  (a b c d : Point) (AD BC AB CD AC : Line)
+  (hassump1 : formParallelogram a d b c AD BC AB CD ∧ distinctPointsOnLine a c AC)
+  : Triangle.area △ a:b:c = Triangle.area △ a:c:d := by
+  obtain ⟨hpara, hac⟩ := hassump1
+  euclid_apply (proposition_34 b a c d AB CD BC AD AC)
+  euclid_finish
+
+end Elements.Book1
